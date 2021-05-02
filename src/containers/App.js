ce68cbacffb0ad80c7b9db23f7e719910d4b8347
Merka95 <you@example.com>
2021-05-03 00:59:49 +0100

    merkapp

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,20 +2,19 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import {robots} from '../components/robots';
 import './App.css';
     
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: [],
+            robots: robots,
             searchfield: ''
         }
     }
-    componentDidMount () {
-        fetch('http://jsonplaceholder.typicode.com/users')
-        .then(response=> response.json()).then(users=>{this.setState({robots:users})});
-    }
+    
+    
     onSearchChange = (event) => {
         this.setState ({searchfield: event.target.value})
     
